e51227c18208b907aecc2f7b7074325bc6cc6f92
Steven Roscoe <112785884+StevenRoscoe@users.noreply.github.com>
2022-12-30 13:39:08 -0600

    Update bestdayever3.sh

--- bestdayever3.sh
+++ bestdayever3.sh
@@ -8,6 +8,6 @@ echo "You're looking good today $name!"
 sleep 2
 echo "You have a great day $name!"
 
-#The dollar sign after the number 1 is what's known as a positional parameter, or argument. While typing in the command
+#The dollar sign before the number 1 is what's known as a positional parameter, or argument. While typing in the command
 #to run the script, anything after ./<script name> will be used as the variable.
 #Ex: ./bestdayever3.sh Steven
